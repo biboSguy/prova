14ed47e8e649ecdc132e8dc400b25b138b59d02c
biboSguy <132067957+biboSguy@users.noreply.github.com>
2024-02-28 12:24:13 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,10 @@
+pontos = 0
+
 def perguntas():
     print("Prova super Difisil")
     while True:
         try:
-            per1 = (input("Você é gay?: "))
+            per1 = input("Você é gay?: ")
             if per1.lower() == 'sim':
                 print("gay")
             elif per1.lower() == 'nao':
@@ -10,12 +12,11 @@ def perguntas():
             else:
                 print("digite valores validos")
                 continue
-            break
         except ValueError:
-            print("Por favor, digite números válidos.")
+            print("a")
         while True:            
             try:
-                per2 = (input("Você é viado?: "))
+                per2 = input("Você é viado?: ")
                 if per2.lower() == 'sim':
                     print("viado")
                 elif per2.lower() == 'nao':
@@ -23,12 +24,11 @@ def perguntas():
                 else:
                     print("digite valores validos")
                     continue
-                break
             except ValueError:
-                print("Por favor, digite valores válidos para a segunda pergunta.")
+                print("a")
             while True:
                 try:
-                    per3 =(input("Você é hetero?: "))
+                    per3 = input("Você é hetero?: ")
                     if per3.lower() == 'sim':
                         print("hetero")
                     elif per3.lower() == 'nao':
@@ -36,10 +36,108 @@ def perguntas():
                     else:
                         print("digite valores validos")
                         continue
-                    break
                 except ValueError:
-                    print("Por favor, digite valores válidos para a segunda pergunta.")
-    
+                    print("a")
+                    while True:            
+                        try:
+                            per4 = input("Você é viado?: ")
+                            if per4.lower() == 'sim':
+                                print("viado")
+                            elif per4.lower() == 'nao':
+                                print("hetero")
+                            else:
+                                print("digite valores validos")
+                                continue
+                        except ValueError:
+                            print("a")
+                            while True:            
+                                try:
+                                    per5 = input("Você é viado?: ")
+                                    if per5.lower() == 'sim':
+                                        print("viado")
+                                    elif per5.lower() == 'nao':
+                                        print("hetero")
+                                    else:
+                                        print("digite valores validos")
+                                        continue
+                                except ValueError:
+                                    print("a.")
+                                    while True:
+                                        try:
+                                            per6 = input("Você é viado?: ")
+                                            if per6.lower() == 'sim':
+                                                print("viado")
+                                            elif per6.lower() == 'nao':
+                                                print("hetero")
+                                            else:
+                                                print("digite valores validos")
+                                                continue
+                                        except ValueError:
+                                            print("a.")
+                                            while True:
+                                                try:
+                                                    per7 = input("Você é viado?: ")
+                                                    if per7.lower() == 'sim':
+                                                        print("viado")
+                                                    elif per7.lower() == 'nao':
+                                                        print("hetero")
+                                                    else:
+                                                        print("digite valores validos")
+                                                        continue
+                                                except ValueError:
+                                                    print("a.")
+                                                    while True:
+                                                        try:
+                                                            per8 = input("Você é viado?: ")
+                                                            if per8.lower() == 'sim':
+                                                                print("viado")
+                                                            elif per8.lower() == 'nao':
+                                                                print("hetero")
+                                                            else:
+                                                                print("digite valores validos")
+                                                                continue
+                                                        except ValueError:
+                                                            print("a.")
+                                                            while True:
+                                                                try:
+                                                                    per9 = input("Você é viado?: ")
+                                                                    if per9.lower() == 'sim':
+                                                                        print("viado")
+                                                                    elif per9.lower() == 'nao':
+                                                                        print("hetero")
+                                                                    else:
+                                                                        print("digite valores validos")
+                                                                        continue
+                                                                except ValueError:
+                                                                    print("a.")
+                                                                    while True:
+                                                                        try:
+                                                                            per10 = input("Você é viado?: ")
+                                                                            if per10.lower() == 'sim':
+                                                                                print("viado")
+                                                                            elif per10.lower() == 'nao':
+                                                                                print("hetero")
+                                                                            else:
+                                                                                print("digite valores validos")
+                                                                                continue
+                                                                        except ValueError:
+                                                                            print("a.")
+                                                                        break
+                                                                    break
+                                                                break
+                                                            break
+                                                        break
+                                                    break
+                                                break
+                                            break
+                                        break
+                                    break
+                                break
+                            break
+                        break
+                    break
+                break
             break
         break
 perguntas()
+
